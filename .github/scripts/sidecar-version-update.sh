9c11d6e0890bd940b0754389e2b04f1f4a2a9ee0
Harish P <harish_p4@dell.com>
2025-07-17 19:04:27 +0530

    updated script

--- .github/scripts/sidecar-version-update.sh
+++ .github/scripts/sidecar-version-update.sh
@@ -15,21 +15,35 @@
 # limitations under the License.
 
 # This script updates the sidecar versions across CSM-operator files
-attacher_ver="$1"
-snapshotter_ver="$2"
-provisioner_ver="$3"
-registrar_ver="$4"
-health_monitor_ver="$5"
-metadata_retriever_ver="$6"
-resizer_ver="$7"
-sdc_ver="$8"
+
+# Fetch the latest release information from the GitHub API for all the sidecars
+provisioner_ver=$(curl --silent "https://api.github.com/repos/kubernetes-csi/external-provisioner/releases/latest" | grep -oP '"tag_name": "\K(.*?)(?=")')
+attacher_ver=$(curl --silent "https://api.github.com/repos/kubernetes-csi/external-attacher/releases/latest" | grep -oP '"tag_name": "\K(.*?)(?=")')
+snapshotter_ver=$(curl --silent "https://api.github.com/repos/kubernetes-csi/external-snapshotter/releases/latest" | grep -oP '"tag_name": "\K(.*?)(?=")')
+registrar_ver=$(curl --silent "https://api.github.com/repos/kubernetes-csi/node-driver-registrar/releases/latest" | grep -oP '"tag_name": "\K(.*?)(?=")')
+health_monitor_ver=$(curl --silent "https://api.github.com/repos/kubernetes-csi/external-health-monitor/releases/latest" | grep -oP '"tag_name": "\K(.*?)(?=")')
+resizer_ver=$(curl --silent "https://api.github.com/repos/kubernetes-csi/external-resizer/releases/latest" | grep -oP '"tag_name": "\K(.*?)(?=")')
+
+# Internal images and hence we will access quay.io/dell repos to get the latest version
+sdc_ver=$(https://quay.io/api/v1/repository/dell/storage/powerflex/sdc/tag/?limit=100 | jq -r '.tags[].name' | sort -V | tail -n 1)
+metadata_retriever_ver=$(https://quay.io/api/v1/repository/dell/container-storage-modules/csi-metadata-retriever/tag/?limit=100 | jq -r '.tags[].name' | sort -V | tail -n 1)
+
+echo "provisioner latest version: $provisioner_ver"
+echo "attacher latest version: $attacher_ver"
+echo "snapshotter latest version: $snapshotter_ver"
+echo "registrar latest version: $registrar_ver"
+echo "health_monitor latest version: $health_monitor_ver"
+echo "resizer latest version: $resizer_ver"
+
+echo "metadata_retriever latest version: $metadata_retriever_ver"
+echo "sdc latest version: $sdc_ver"
 
 cd $GITHUB_WORKSPACE
 
 for sidecar in {attacher,provisioner,snapshotter,registrar,resizer,external-health-monitor,sdc,metadata-retriever}
   do
     echo "Updating sidecar version for -->> $sidecar"
-    old_sidecar_ver=$(cat operatorconfig/driverconfig/common/default.yaml | grep $sidecar | egrep 'registry.k8s.io|quay.io'  | awk '{print $2}')
+    old_sidecar_ver=$(cat ./operatorconfig/driverconfig/common/default.yaml | grep $sidecar | egrep 'registry.k8s.io|quay.io'  | awk '{print $2}')
     old_sidecar_sub_string=$(echo $old_sidecar_ver | awk -F':' '{print $1}')
 
     files_to_be_modified=$(grep -rl $old_sidecar_ver)
